6d3b2aa858eff7b829d5d4c9b88ec86a0a514b44
SOOHOKIM <kshorg@naver.com>
2021-07-02 21:37:58 +0900

    Update baekjoon_2468.py

--- 01.baekjoon/baekjoon_2468.py
+++ 01.baekjoon/baekjoon_2468.py
@@ -1,3 +1,4 @@
+# ver1
 def dfs(x, y, matrix):
     # 방문한 위치 0으로 변경
     matrix[x][y] = 0
@@ -42,3 +43,48 @@ for k in range(1, max_number+1):
 
     answer = max(answer, count)
 print(answer)
+
+# ver2
+import sys
+from copy import deepcopy
+
+sys.setrecursionlimit(10**9)
+
+N = int(input())
+graph = [[0]*N for _ in range(N)]
+min_number = 101
+max_number = 0
+for i in range(N):
+    graph[i] = list(map(int, input().split()))
+    if max(graph[i]) > max_number:
+        max_number = max(graph[i])
+    if min(graph[i]) < min_number:
+        min_number = min(graph[i])
+
+def dfs(graph, x, y):
+    graph[x][y] = 0
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, -1, 1]
+    for idx in range(4):
+        x0 = x + dx[idx]
+        y0 = y + dy[idx]
+        if (0 <= x0 < N) and (0 <= y0 < N) and (graph[x0][y0] != 0):
+            dfs(graph, x0, y0)
+
+count_list = []
+number = min_number-1
+while number < max_number:
+    temp_graph = deepcopy(graph)
+    count = 0
+    for i in range(N):
+        for j in range(N):
+            if temp_graph[i][j] <= number:
+                temp_graph[i][j] = 0
+    for x in range(N):
+        for y in range(N):
+            if temp_graph[x][y] != 0:
+                dfs(temp_graph, x, y)
+                count += 1
+    count_list.append(count)
+    number += 1
+print(max(count_list))
